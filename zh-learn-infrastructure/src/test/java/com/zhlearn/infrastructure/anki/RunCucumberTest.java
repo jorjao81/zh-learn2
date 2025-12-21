8f2e3a5ba31c95fb9395d9a61107185f45605919
Paulo Schreiner <paulo.schreiner@gmail.com>
2025-12-21 10:28:23 +0100

    fix: resolve cucumber test discovery warnings
    Use @SelectPackages instead of @SelectClasspathResource to avoid
    deprecation warnings about classpath resource selectors in Cucumber
    JUnit Platform integration.

--- zh-learn-infrastructure/src/test/java/com/zhlearn/infrastructure/anki/RunCucumberTest.java
+++ zh-learn-infrastructure/src/test/java/com/zhlearn/infrastructure/anki/RunCucumberTest.java
@@ -1,16 +1,14 @@
 package com.zhlearn.infrastructure.anki;
 
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
-
-import io.cucumber.junit.platform.engine.Constants;
 
 @Suite
 @IncludeEngines("cucumber")
-@SelectClasspathResource("features")
-@ConfigurationParameter(
-        key = Constants.GLUE_PROPERTY_NAME,
-        value = "com.zhlearn.infrastructure.anki")
+@SelectPackages("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.zhlearn.infrastructure.anki")
 public class RunCucumberTest {}
